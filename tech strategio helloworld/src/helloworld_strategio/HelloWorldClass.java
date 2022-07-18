cf5703a5f352c558bcc2aa9ef87c4dde663ae764
Poojitha <poojitha.jakkasingaraiah@strategio.com>
2022-07-18 09:05:55 -0700

    First commit to repo

---
+++ tech strategio helloworld/src/helloworld_strategio/HelloWorldClass.java
@@ -0,0 +1,10 @@
+package helloworld_strategio;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
